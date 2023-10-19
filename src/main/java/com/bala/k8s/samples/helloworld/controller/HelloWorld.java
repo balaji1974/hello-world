1fa653dc9e19ea8331fb54314956720220f97f98
Balaji Thiagarajan <99256590+balaji-ajex@users.noreply.github.com>
2023-10-19 21:09:47 +0300

    commit the correct folder

--- src/main/java/com/bala/k8s/samples/helloworld/controller/HelloWorld.java
+++ src/main/java/com/bala/k8s/samples/helloworld/controller/HelloWorld.java
@@ -2,25 +2,15 @@ package com.bala.k8s.samples.helloworld.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bala.k8s.samples.helloworld.dto.Employee;
 
 @RestController
-@RequestMapping("sample")
 public class HelloWorld {
 	
-	@Autowired
-	Employee employee;
 	
-	@GetMapping("/") // For checking the readiness and liveliness of the microservices from the yaml file 
-	public String imHealthy() {
-		return "{healthy:true}";
-	}
-	
-	@GetMapping(value="/employee", produces = "application/json")
-	public Employee getEmployee() {
-		return employee;
+	@GetMapping("/helloworld") 
+	public String helloWorld() {
+		return "Hello World";
 	}
 }
